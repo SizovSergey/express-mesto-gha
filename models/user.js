8db5e6404107d332f8ddb32d426c0545c7fe9159
SergeySizov <vpnsizov@yandex.ru>
2023-06-30 22:24:40 +0300

    вторая попытка пройти тесты

--- models/user.js
+++ models/user.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    required: true,
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
+    required: true,
   },
   avatar: {
     type: String,
