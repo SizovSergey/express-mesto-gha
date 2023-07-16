4baf472c5eb309582d063f857103ee29979ebfa5
SergeySizov <vpnsizov@yandex.ru>
2023-07-16 19:03:54 +0300

    отправляю на тесты

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,4 @@
 const express = require('express');
-
-const {
-  celebrate, Joi, Segments,
-} = require('celebrate');
-
 const router = express.Router();
 
 const cardsRouter = require('./cards');
@@ -12,14 +7,7 @@ const loginRouter = require('./signIn');
 const registerRouter = require('./signUp');
 
 router.use('/signin', loginRouter);
-router.use('/signup', celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
-    about: Joi.string().min(2).max(30).default('Исследователь'),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), registerRouter);
+router.use('/signup', registerRouter);
 
 router.use('/cards', cardsRouter);
 router.use('/users', usersRouter);
